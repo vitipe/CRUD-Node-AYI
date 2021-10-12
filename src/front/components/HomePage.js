c9a2aaf75f49de5d73142ba6a6b3dea445cc0a14
vitipe <victorcasbas@gmail.com>
2021-10-12 10:58:39 -0300

    Adds instructions to readme

--- src/front/components/HomePage.js
+++ src/front/components/HomePage.js
@@ -8,6 +8,12 @@ export default function () {
       <p>
         App CRUD para Capacitación Node de AYI
       </p>
+      <p>
+        Levantar el servidor con "node dist/bundle.back"
+      </p>
+      <p>
+        Para edición, compilar el back de webpack y el front de webpack con "npm run compilar:back" y "npm run compilar:front"
+      </p>
     </>
   );
 }
